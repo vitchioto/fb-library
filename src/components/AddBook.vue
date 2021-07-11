361111e137d196bb469a2cc2b44b6f31bb37d78d
vvladovic <vvladovic@gvc-plc.com>
2021-07-11 23:45:29 +0200

    Adding validation to AddBook

--- src/components/AddBook.vue
+++ src/components/AddBook.vue
@@ -19,37 +19,79 @@
           <input
             type="text"
             class="input"
+            :class="{'is-danger':
+              errorMaxLength.includes('title') || errorRequired.includes('title')}"
             :placeholder="$t('placeholderTitle')"
             v-model="title"
           >
         </label>
+        <div
+          v-if="errorMaxLength.includes('title')"
+          class="error"
+          v-html="$t('errorMaxLength')"
+        />
+        <div
+          v-if="errorRequired.includes('title')"
+          class="error"
+          v-html="$t('errorRequired')"
+        />
         <label class="label">
           {{ $t('author') }}
           <input
             type="text"
             class="input"
+            :class="{'is-danger':
+              errorMaxLength.includes('author') || errorRequired.includes('author')}"
             :placeholder="$t('placeholderAuthor')"
             v-model="author"
           >
         </label>
+        <div
+          v-if="errorMaxLength.includes('author')"
+          class="error"
+          v-html="$t('errorMaxLength')"
+        />
+        <div
+          v-if="errorRequired.includes('author')"
+          class="error"
+          v-html="$t('errorRequired')"
+        />
         <label class="label">
           {{ $t('theme') }}
           <input
             type="text"
             class="input"
+            :class="{'is-danger': errorMaxLength.includes('theme')}"
             :placeholder="$t('placeholderTheme')"
             v-model="theme"
           >
         </label>
+        <div
+          v-if="errorMaxLength.includes('theme')"
+          class="error"
+          v-html="$t('errorMaxLength')"
+        />
         <label class="label">
           {{ $t('language') }}
           <input
             type="text"
             class="input"
+            :class="{'is-danger':
+              errorMaxLength.includes('language') || errorRequired.includes('language')}"
             :placeholder="$t('placeholderLanguage')"
             v-model="language"
           >
         </label>
+        <div
+          v-if="errorMaxLength.includes('language')"
+          class="error"
+          v-html="$t('errorMaxLength')"
+        />
+        <div
+          v-if="errorRequired.includes('language')"
+          class="error"
+          v-html="$t('errorRequired')"
+        />
         <button
           class="button is-primary"
           @click="submitBook()"
@@ -70,6 +112,8 @@ export default {
   data() {
     return {
       author: '',
+      errorMaxLength: [],
+      errorRequired: [],
       isbn: '',
       language: '',
       theme: '',
@@ -81,14 +125,31 @@ export default {
       this.$emit('closeForm');
     },
     async submitBook() {
-      const book = {
-        author: this.author,
-        language: this.language,
-        theme: this.theme,
-        title: this.title,
-      };
-      await this.$store.dispatch('submitBook', book);
-      this.closeForm();
+      const isValid = this.validate();
+      if (isValid) {
+        const book = {
+          author: this.author,
+          language: this.language,
+          theme: this.theme,
+          title: this.title,
+        };
+        await this.$store.dispatch('submitBook', book);
+        this.closeForm();
+      }
+    },
+    validate() {
+      this.errorMaxLength = [];
+      this.errorRequired = [];
+      if (!this.author.length) this.errorRequired.push('author');
+      if (!this.language.length) this.errorRequired.push('language');
+      if (!this.title.length) this.errorRequired.push('title');
+
+      if (this.author.length > 100) this.errorMaxLength.push('author');
+      if (this.language.length > 100) this.errorMaxLength.push('language');
+      if (this.theme.length > 100) this.errorMaxLength.push('theme');
+      if (this.title.length > 100) this.errorMaxLength.push('title');
+
+      return !(this.errorMaxLength.length + this.errorRequired.length);
     },
   },
 };
@@ -97,5 +158,10 @@ export default {
 <style lang="scss">
 .label {
   color: #fff;
+}
+
+.error {
+  color: hsl(348, 100%, 61%);
+  margin-bottom: 10px;
 }
 </style>
