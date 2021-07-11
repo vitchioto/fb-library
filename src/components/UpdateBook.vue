c30241ecec4ed69f634db007efe9c2ff731a94fb
vvladovic <vvladovic@gvc-plc.com>
2021-07-11 23:58:20 +0200

    Adding validation to UpdateBook

--- src/components/UpdateBook.vue
+++ src/components/UpdateBook.vue
@@ -26,37 +26,79 @@
           <input
             type="text"
             class="input"
+            :class="{'is-danger':
+              errorMaxLength.includes('title') || errorRequired.includes('title')}"
             :placeholder="$t('placeholderTitle')"
             v-model="title"
           >
         </label>
+        <div
+          v-if="errorMaxLength.includes('title')"
+          class="error"
+          v-html="$t('errorMaxLength')"
+        />
+        <div
+          v-if="errorRequired.includes('title')"
+          class="error"
+          v-html="$t('errorRequired')"
+        />
         <label class="label">
           {{ $t('author') }}
           <input
             type="text"
             class="input"
+            :class="{'is-danger':
+              errorMaxLength.includes('author') || errorRequired.includes('author')}"
             :placeholder="$t('placeholderAuthor')"
             v-model="author"
           >
         </label>
+        <div
+          v-if="errorMaxLength.includes('author')"
+          class="error"
+          v-html="$t('errorMaxLength')"
+        />
+        <div
+          v-if="errorRequired.includes('author')"
+          class="error"
+          v-html="$t('errorRequired')"
+        />
         <label class="label">
           {{ $t('theme') }}
           <input
             type="text"
             class="input"
+            :class="{'is-danger': errorMaxLength.includes('theme')}"
             :placeholder="$t('placeholderTheme')"
             v-model="theme"
           >
         </label>
+        <div
+          v-if="errorMaxLength.includes('theme')"
+          class="error"
+          v-html="$t('errorMaxLength')"
+        />
         <label class="label">
           {{ $t('language') }}
           <input
             type="text"
             class="input"
+            :class="{'is-danger':
+              errorMaxLength.includes('language') || errorRequired.includes('language')}"
             :placeholder="$t('placeholderLanguage')"
             v-model="language"
           >
         </label>
+        <div
+          v-if="errorMaxLength.includes('language')"
+          class="error"
+          v-html="$t('errorMaxLength')"
+        />
+        <div
+          v-if="errorRequired.includes('language')"
+          class="error"
+          v-html="$t('errorRequired')"
+        />
         <div class="buttons">
           <button
             class="button is-primary"
@@ -90,6 +132,8 @@ export default {
   data() {
     return {
       author: '',
+      errorMaxLength: [],
+      errorRequired: [],
       language: '',
       renterFbId: '',
       theme: '',
@@ -117,23 +161,40 @@ export default {
       this.closeForm();
     },
     async updateBook() {
-      let renterFbId = '';
-      let renterName = '';
-      if (this.renterFbId) {
-        const renter = this.friends.find((item) => item.id === this.renterFbId);
-        renterFbId = renter.id;
-        renterName = renter.name;
+      const isValid = this.validate();
+      if (isValid) {
+        let renterFbId = '';
+        let renterName = '';
+        if (this.renterFbId) {
+          const renter = this.friends.find((item) => item.id === this.renterFbId);
+          renterFbId = renter.id;
+          renterName = renter.name;
+        }
+        const book = {
+          author: this.author,
+          language: this.language,
+          renterFbId,
+          renterName,
+          theme: this.theme,
+          title: this.title,
+        };
+        await this.$store.dispatch('updateBook', [book, this.selectedBook.id]);
+        this.closeForm();
       }
-      const book = {
-        author: this.author,
-        language: this.language,
-        renterFbId,
-        renterName,
-        theme: this.theme,
-        title: this.title,
-      };
-      await this.$store.dispatch('updateBook', [book, this.selectedBook.id]);
-      this.closeForm();
+    },
+    validate() {
+      this.errorMaxLength = [];
+      this.errorRequired = [];
+      if (!this.author.length) this.errorRequired.push('author');
+      if (!this.language.length) this.errorRequired.push('language');
+      if (!this.title.length) this.errorRequired.push('title');
+
+      if (this.author.length > 100) this.errorMaxLength.push('author');
+      if (this.language.length > 100) this.errorMaxLength.push('language');
+      if (this.theme.length > 100) this.errorMaxLength.push('theme');
+      if (this.title.length > 100) this.errorMaxLength.push('title');
+
+      return !(this.errorMaxLength.length + this.errorRequired.length);
     },
   },
 };
@@ -142,5 +203,9 @@ export default {
 <style lang="scss">
 .label {
   color: #fff;
+}
+.error {
+  color: hsl(348, 100%, 61%);
+  margin-bottom: 10px;
 }
 </style>
